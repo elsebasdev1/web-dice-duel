4cb351fbac40d9ad50a8160e8a89b3f6f051aba3
Sebastián Sinchi Naula <151038925+elsebasdev1@users.noreply.github.com>
2025-04-01 09:00:51 -0500

    General improvements

--- script.js
+++ script.js
@@ -39,22 +39,22 @@ const cube = document.querySelector('.cube');
 
                 switch(randomValue) {
                     case 1:             
-                        cube.style.transform = `translateY(300px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        cube.style.transform = `translateY(200px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)`;
                         break;
                     case 2:
-                        cube.style.transform = `translateY(300px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        cube.style.transform = `translateY(200px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)`;
                         break;
                     case 3:
-                        cube.style.transform = `translateY(300px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)`;
+                        cube.style.transform = `translateY(200px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)`;
                         break;
                     case 4:
-                        cube.style.transform = `translateY(300px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)`;
+                        cube.style.transform = `translateY(200px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)`;
                         break;
                     case 5:
-                        cube.style.transform = `translateY(300px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        cube.style.transform = `translateY(200px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)`;
                         break;
                     case 6:
-                        cube.style.transform = `translateY(300px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)`;
+                        cube.style.transform = `translateY(200px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)`;
                         break;
                 }
         }, 300);
